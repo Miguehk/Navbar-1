39e0ea59dd9e93ca42bd2738a8cec8e7b83c4e34
Miguel Paniagua <mistergamer180@gmail.com>
2022-06-03 06:13:20 -0600

    Agregamos comentarios

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
+    // Declaramos las variables
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('toggle-menu'),
     closeMenu =document.getElementById('close-menu')
 
 
+
+    // Abre el menu
 toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
 
-
+    // Cierra el menu
 closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
